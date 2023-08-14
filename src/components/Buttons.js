f87eb1affb50e4e5e2af87b48fc1bfdbd706f9f9
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2023-08-13 20:26:39 -0500

    Finished Assignment

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,17 +1,17 @@
 import satData from "./satData";
 import styles from './styling.css'
 
-const Buttons = (filterByType, setSat, displaySats) => {
-  {displaySats.map((sat,id) => {
-    return (
-      <div className="flex-container">
-      <button onClick={() => filterByType(sat)} key = {id}>{sat} Orbit</button>
-      </div>
-  );
-  })}
+const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
-    <div>
-    <button onClick={setSat(satData)}></button>
+    <div className="flex-container">
+    {displaySats.map((sat,id) => {
+      return (
+        <div>
+        <button onClick={() => filterByType(sat)} key = {id}>{sat} Orbit</button>
+        </div>
+    );
+    })}
+    <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   )
 };
